b8049959c5d10b997025aee404bd275eacbb7fd1
bugger0330 <bugger0330@naver.com>
2022-04-19 13:11:49 +0900

    데이터베이스에서 이미지명 불러와서 페이지에 쏴주는것

--- JspStudy_7290/src/main/java/repository/FileDao.java
+++ JspStudy_7290/src/main/java/repository/FileDao.java
@@ -2,7 +2,10 @@ package repository;
 
 import java.util.ArrayList;
 
+import repository.file.FileEntity;
+
 public interface FileDao {
 
 	public int uploadFile(ArrayList<String> fileList);
+	public ArrayList<FileEntity> getImgListAll();
 }
